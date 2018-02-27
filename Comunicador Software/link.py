2e74e7a2f4b4964821adbd75315680faa8b238d7
Henrique Mauler Borges <henriquemauler@gmail.com>
2018-02-27 19:17:35 -0300

    att 27/02/2018

--- Comunicador Software/link.py
+++ Comunicador Software/link.py
@@ -14,23 +14,48 @@ ser = serial.Serial();
 def t1():
     while True:
         mensagem = input('digite a mensagem: ');
-        print('instrução {} enviada com sucesso!'.format(mensagem));
+        
         if(mensagem == 'sair'):
              print('\r\n');
              break;
         elif mensagem == 'repeat':
             soquete = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            while True:                    
-                soquete.sendto(mensagem.encode('utf-8'), (localhost, serial1));
-                time.sleep(3);
+            soquete.sendto('enegia01'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia02'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia03'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia04'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia05'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia06'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia07'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia08'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia09'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia10'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia11'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+            soquete.sendto('enegia12'.encode('utf-8'), (localhost, serial1));
+            time.sleep(0.5);
+
+
         else:
             soquete = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             soquete.sendto(mensagem.encode('utf-8'), (localhost, serial1));
+            print('instrução {} enviada com sucesso!'.format(mensagem));
             
         
 def t2():
     soquete = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
     soquete.bind((localhost, serial2));
+    print('entrando no buffer')
     while True:
         print('recebido: {}'.format(soquete.recv(30)));
 
@@ -44,7 +69,7 @@ def iniciarUdp():
     thread1.join();
 
 def configurarSerial():
-    baundrate = int(input('defina o baundrate '));
+    baundrate = int(input('\r\ndefina o baundrate '));
     porta = input('qual a porta? ')
     global ser;
     ser.baundrate = baundrate;
@@ -53,7 +78,7 @@ def configurarSerial():
     try:
         ser.open()
         if ser.is_open:
-            print('porta aberta com sucesso')
+            print('\r\nporta aberta com sucesso')
             status = True;
             
     finally:
@@ -61,26 +86,20 @@ def configurarSerial():
 
 def serialMensagem():
     global ser;
-    asc = input('digite a instrução de envio: ')
+    asc = input('\r\ndigite a instrução de envio: ')
     ser.write(asc.encode())
     
     
 def readSerial():
-     print(ser.read(3));
+     print(ser.readline);
 
 #o programa
 
 print('Programa de comunicaçao entre canal udp e serial\r\n');
-ser.baundrate = 115200;
-ser.port = 'COM5'
-ser.open();
+
 while True:
-    comando = input('digite o comando que deseja executa: ');  
-    if comando == 'ajuda':
-        print('digite \'ajuda\' para exibir esta lista.');
-        print('digite \'configurarUdp\' para realizar configurações do servidor udp, tal como IP e portas.');
-        print('digite \'configurarSerial\' para realizar configurações do canal serial, como baundrate e a porta manualmente.');
-    elif comando == 'udp':
+    comando = input('\r\ndigite o comando que deseja executa: ');  
+    if comando == 'conf udp':
               x = input('digite o IP que será utilizado. (padrão: {})'.format(localhost))
               if x != "":
                   localhost = x;
@@ -90,15 +109,15 @@ while True:
               x = input('digite a porta para a comunicaçao oposta, entre udp e serial. (padrão: {}'.format(serial2))
               if x != "":
                   serial2 = x;
-    elif comando == 'configurarserial':
+    elif comando == 'conf serial':
         configurarSerial()
-    elif comando == 'initudp':
+    elif comando == 'udp':
         iniciarUdp();
         time.sleep(0.5);
-    elif comando == 'initserial':
+    elif comando == 'serial':
         serialMensagem();
     elif comando == 'readserial':
-        readSerial();
+        readSerial();      
     else:
         print('comando não encontrado\r\n');
 
